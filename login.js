596a104e532531bc7ce58630dc2f77df2dbace77
Boiragedada <boiragegaming@gmail.com>
2026-03-13 22:03:51 +0600

    Third commit

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ const password=document.getElementById("password").value
 
 if(username==="admin" && password==="admin123"){
 
-window.location.href="index.html"
+window.location.href="main.html"
 
 }else{
 
